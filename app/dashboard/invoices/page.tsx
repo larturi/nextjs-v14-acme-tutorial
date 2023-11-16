2a9271b8b5e43e302b5d687f434db06e1b56b105
Leandro Arturi <lea.arturi@gmail.com>
2023-11-16 14:23:57 -0300

    Adding metadata

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -1,11 +1,16 @@
+import { Metadata } from 'next';
+import { montserrat } from '@/app/ui/fonts';
 import Pagination from '@/app/ui/invoices/pagination';
 import Search from '@/app/ui/search';
 import Table from '@/app/ui/invoices/table';
 import { CreateInvoice } from '@/app/ui/invoices/buttons';
-import { montserrat } from '@/app/ui/fonts';
 import { InvoicesTableSkeleton } from '@/app/ui/skeletons';
 import { Suspense } from 'react';
 import { fetchInvoicesPages } from '@/app/lib/data';
+
+export const metadata: Metadata = {
+   title: 'Invoices',
+};
 
 export default async function Page({
    searchParams,
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,19 +1,30 @@
-import "@/app/ui/global.css";
-import { montserrat } from "./ui/fonts";
+import '@/app/ui/global.css';
+
+import { Metadata } from 'next';
+import { montserrat } from './ui/fonts';
+
+export const metadata: Metadata = {
+   title: {
+      template: '%s | Acme Dashboard',
+      default: 'Acme Dashboard',
+   },
+   description: 'The official Next.js Learn Dashboard built with App Router.',
+   metadataBase: new URL('https://next-learn-dashboard.vercel.sh'),
+};
 
 export default function RootLayout({
-  children,
+   children,
 }: {
-  children: React.ReactNode;
+   children: React.ReactNode;
 }) {
-  return (
-    <html lang="en">
-      <body
-        suppressHydrationWarning={true}
-        className={`${montserrat.className} antialiased`}
-      >
-        {children}
-      </body>
-    </html>
-  );
+   return (
+      <html lang='en'>
+         <body
+            suppressHydrationWarning={true}
+            className={`${montserrat.className} antialiased`}
+         >
+            {children}
+         </body>
+      </html>
+   );
 }
